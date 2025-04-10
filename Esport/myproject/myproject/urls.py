f28f41ab4c7b32c20f7189a636c0a948211cbb78
Maohsun.Wu <maohsun.wu@wanin.tw>
2025-04-10 14:58:49 +0800

    更新專案urls

--- Esport/myproject/myproject/urls.py
+++ Esport/myproject/myproject/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('game_results/', include('game_results.urls')),
-]
+    path('', include('game_results.urls')), # 包含 game_results 應用程式的 URL
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
